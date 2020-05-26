fd3a1dab979767b50e1b879b9c5932812daa2a33
Paweł Kołodziejski <aquadran@users.sourceforge.net>
2020-05-26 10:29:50 +0200

    improvements to setup env

--- linux-omap4/_env.sh
+++ linux-omap4/_env.sh
@@ -1,7 +1,7 @@
 toolsroot="${OE_BASE}/build-${DISTRO}/tmp/sysroots/`uname -m`-`uname -s | awk '{print tolower($0)}'`/usr"
 sysroot="${OE_BASE}/build-${DISTRO}/tmp/sysroots/armv7a-hf-linux-gnueabi"
 if [ ! `echo ${PATH} | grep armv7a-hf/bin` ]; then
-	export PATH="${PATH}:${toolsroot}/bin:${toolsroot}/armv7a-hf/bin"
+	export PATH="${OE_BASE}/oe/bin:${PATH}:${toolsroot}/bin:${toolsroot}/armv7a-hf/bin"
 fi
 export HOST_INC="-I${toolsroot}/include"
 export CROSS_COMPILE=arm-linux-gnueabi-
