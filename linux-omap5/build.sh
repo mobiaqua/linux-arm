348c94ca83144e2af8fe1a412e7260cd039e2d43
Paweł Kołodziejski <aquadran@users.sourceforge.net>
2020-06-04 21:35:30 +0200

    only igep0050

--- linux-omap5/build.sh
+++ linux-omap5/build.sh
@@ -6,4 +6,4 @@
 
 make ARCH=arm uImage -j4
 #make ARCH=arm modules -j4
-make ARCH=arm omap3-igep0030.dtb omap4-panda.dtb omap4-panda-es.dtb omap5-igep0050.dtb am57xx-beagle-x15-revb1.dtb
+make ARCH=arm omap5-igep0050.dtb
