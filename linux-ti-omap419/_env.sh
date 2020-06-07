e1ccb353fe33898970f6bb05221fd9ce37de3926
Paweł Kołodziejski <aquadran@users.sourceforge.net>
2020-06-07 15:43:13 +0200

    Added pkg-config path

--- linux-ti-omap419/_env.sh
+++ linux-ti-omap419/_env.sh
@@ -3,8 +3,8 @@ sysroot="${OE_BASE}/build-${DISTRO}/tmp/sysroots/armv7a-hf-linux-gnueabi"
 if [ ! `echo ${PATH} | grep armv7a-hf/bin` ]; then
 	export PATH="${OE_BASE}/oe/bin:${PATH}:${toolsroot}/bin:${toolsroot}/armv7a-hf/bin"
 fi
-ssl_cflags=`pkg-config --cflags libssl`
-ssl_ldflags=`pkg-config --libs libssl`
+ssl_cflags=`${toolsroot}/bin/pkg-config --cflags libssl`
+ssl_ldflags=`${toolsroot}/bin/pkg-config --libs libssl`
 export HOSTCFLAGS="${ssl_cflags} -I${toolsroot}/include"
 export HOSTLDFLAGS="${ssl_ldflags} -I${toolsroot}/lib"
 export CROSS_COMPILE=arm-linux-gnueabi-
