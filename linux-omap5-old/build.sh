8f8d855ca183064efe1d98c2771c7169e8708d5c
Paweł Kołodziejski <aquadran@users.sourceforge.net>
2020-06-12 22:27:08 +0200

    Added old kernel omap5

---
+++ linux-omap5-old/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+. _env.sh
+
+#export KCFLAGS=-DDEBUG
+
+make ARCH=arm uImage -j4
+make ARCH=arm modules -j4
+make ARCH=arm omap5-igep0050.dtb
---
+++ linux-omap5-old/setup-linux.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+[ -z "${OE_BASE}" ] && echo "Script require OE_BASE and DISTRO configured by OE environment." && exit 1
+
+echo
+echo "Preparing linux sources..."
+echo
+
+if [ ! -e linux/Makefile ]; then
+	curdir=$PWD
+	cd ${OE_BASE}/build-${DISTRO} &&
+		source env.source &&
+		${OE_BASE}/bb/bin/bitbake linux-omap5-old -cclean &&
+		${OE_BASE}/bb/bin/bitbake linux-omap5-old -cconfigure && {
+			cd ${curdir}
+			echo
+			echo "Copy linux sources..."
+			echo
+			cp -R ${OE_BASE}/build-${DISTRO}/tmp/work/board-tv-linux-gnueabi/linux-omap5-old*/git/ linux/
+			cp build.sh clean.sh debug.sh menu.sh _env.sh linux/
+			chmod +x linux/*.sh
+			echo ;echo "--- Setup done ---"; echo
+		}
+else
+	echo
+	echo "Nothing to do..."
+	echo
+fi
+
+cd linux
